75c6e4f92bb3c4fb8a722c468487b59e8c8326bd
eunbin00 <eun61n@gmail.com>
2023-08-22 10:42:21 +0900

    #1 - Edit main.dart and add color constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:online_graduation/utils/color_constant.dart';
+
+import 'navigation.dart';
+import 'screens/login/login_screen.dart';
+import 'screens/setting/setting_screen.dart';
 
 void main() {
   runApp(const OnlineGraduationApp());
@@ -11,11 +16,25 @@ class OnlineGraduationApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Online Graduation App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.black,
+          primary: Colors.black,
+          secondary: Colors.purple,
+        ),
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: BLACK,
+              fontFamily: 'SCoreDream',
+            ),
         useMaterial3: true,
       ),
-      home: const Placeholder(),
+      initialRoute: '/login',
+      routes: {
+        '/': (context) => const Navigation(),
+        '/login': (context) => const LoginScreen(),
+        '/setting': (conetxt) => const SettingScreen(),
+      },
     );
   }
 }
---
+++ lib/utils/color_constant.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color PINK = Color(0xFFF11A7B);
+const Color PURPLE = Colors.purple;
+const Color WHITE = Colors.white;
+const Color BLACK = Colors.black;
+const Color SHADOW_BLACK = Colors.black87;
